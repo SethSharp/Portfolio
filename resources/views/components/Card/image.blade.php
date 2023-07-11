d0ec07dac8e8e8f2d68b71a3f73ad41d55eab5cb
Seth Sharp <sesharp@outlook.com>
2023-07-11 13:37:17 +1000

    Change how the project card works and design changes and finalisation of descriptions

--- resources/views/components/Card/image.blade.php
+++ resources/views/components/Card/image.blade.php
@@ -1,8 +1,6 @@
 @props(['image' => 'blank'])
 
-<div class="w-[14rem] h-[14rem]
-            transition ease-in-out delay-300
-            hover:-translate-y-1 hover:scale-[1.75] duration-700">
+<div class="w-[14rem] h-[14rem]">
     <img class="object-contain h-full w-96 p-4"
          src="/images/{{$image}}"
          alt="about-picture"/>
--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -9,78 +9,59 @@
 </head>
 
 <body class="mg-0 pd-0">
-    <x-nav-bar projects="text-black border-black" ></x-nav-bar>
-    <div class="h-6"></div>
-    <x-title title1="My" title2="Projects"></x-title>
-    <div class="w-full sm:mt-5 py-3 sm:py-5 px-10 sm:px-28">
-        @component('components.project',
-                    ['title' => 'Time Recorder',
-                     'desc' => '
-                        I made a habit in 2021 of making my own software applications that
-                        would help with uni. This is one of those that I haven`t broken
-                        and was able to finish off. This project allows me to effectively
-                        record the time I would spend on subjects. Saving it to MongoDB
-                        and allow me to analyse over the weeks how my work rate would
-                        change from week to week.
-                     ',
-                     'technology' => ['EJS',
-                                      'Node/Express',
-                                      'MongoDB'],
-                     'link' => 'TimeRecorder',
-                     'dur' => '2 months',
-                     'img_1' => 'TR/MainRecording.jpg',
-                     'img_2' => 'TR/TotalTimes.jpg'
-        ]) @endcomponent
-        @component('components.project',
-                ['title' => 'YouHub',
-                 'desc' => '
-                    This project was built for a subject, which taught us the MVC,
-                    Webpack integration and using API requests. This project built
-                    a page which would allow the user to login view Facebook and based
-                    on the pages they have liked would make a call to the Youtube API
-                    to get the videos from these creators.
-                 ',
-                 'technology' => ['JS',
-                                  'Facebook SDK',
-                                  'Webpack',
-                                  'NodeJS',
-                                  'YouTube Data API v3'],
-                 'link' => 'YouHub',
-                 'dur' => '2 months',
-                 'img_1' => '/YH/main.png',
-                 'img_2' => '/YH/videos.png',
-        ]) @endcomponent
-        @component('components.project',
-                    ['title' => 'Blog Page',
-                     'desc' => '
-                        This Blog Page was made as a project at the Coding Labs
-                        internship. This project showed me how useful Laravel is
-                        to integrate a database and make relationships,
-                        user authentication and authorisation and implementing all this
-                        in a MVC format.
-                     ',
-                     'technology' => ['Laravel',
-                                      'TailwindCSS',
-                                      'Database Design'],
-                     'link' => 'BlogPage',
-                     'dur' => '2 weeks',
-                     'img_1' => 'BP/home.jpg',
-                     'img_2' => 'BP/post.jpg',
-        ]) @endcomponent
-        @component('components.project',
-                    ['title' => 'Aussie Sport Knowledge',
-                     'desc' => '
-                       This was another project from uni and introduced me to web frameworks.
-                       Originally this was an app I made with Xcode back in high-school, which was
-                       actually live for a year or so before it was too expensive to keep up.
-                     ',
-                     'technology' => ['Angular/Ionic',
-                                      'TypeScript'],
-                     'link' => 'Aussie-Sport-Knowledge',
-                     'dur' => '2 months',
-                     'img_1' => 'ASK/menu.png',
-                     'img_2' => 'ASK/game.png'
-        ]) @endcomponent
-    </div>
+
+@php
+    $technologyList = [
+        ['EJS', 'Node/Express', 'MongoDB'],
+        ['JS', 'Webpack', 'NodeJs', 'YouTube Data API v3'],
+        ['Laravel', 'TailwindCSS', 'Database Design'],
+        ['Angular/Ionic', 'TypeScript'],
+    ];
+@endphp
+
+<x-nav-bar projects="text-black border-black"></x-nav-bar>
+<div class="h-6"></div>
+<x-title title1="My" title2="Projects"></x-title>
+<div class="w-full sm:mt-5 py-3 sm:py-5 px-10 sm:px-28">
+    <x-project.card
+        title="Time Recorder"
+        link="TimeRecorder"
+    >
+        This application used <b> EJS </b>, <b> Node/Express</b> and <b> Mongo </b> database
+        to construct a basic API application to keep track of time, for certain subjects.
+        This is a tool I used during university to keep track of the amount of time I
+        dedicated to subjects.
+    </x-project.card>
+
+    <x-project.card
+        title="YouHub"
+        link="YouHub"
+    >
+        This project was built for a subject during university, which taught us about <b> MVC</b>,
+        <b> Webpack</b> and implementing that with the <b> YouTube API </b>. This project built
+        a page which would allow the user to login via <b> Facebook </b> and based
+        on the pages they have liked, it would make a call to the Youtube API
+        to get the videos from the pages they had liked (requirement of the project was to implement
+        any 2 APIs).
+    </x-project.card>
+
+    <x-project.card
+        title="Blog Page"
+        link="BlogPage"
+    >
+        The Blog Page project was a task during placement with <b> Coding Labs </b> to implement the knowledge
+        we had learnt from an internal bootcamp. Which involved <b> CRUD</b> with Laravel, <b> User Authentication</b>
+        and <b> Authorisation</b>. This project taught me how useful and powerful Laravel is.
+    </x-project.card>
+
+    <x-project.card
+        title="Aussie Sport Knowledge"
+        link="Aussie-Sport-Knowledge"
+    >
+        This was another project from university and introduced us to web frameworks, in this case
+        it was <b> Angular </b> paired with <b> Ionic </b>. Originally this was an app I made with
+        Xcode back in high-school, which went live for a short time on the App Store.
+    </x-project.card>
+</div>
 </body>
 </html>
