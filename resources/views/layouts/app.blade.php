99de583ae902c78a0f8006dad7319159c0683846
Seth Sharp <sesharp@outlook.com>
2023-11-03 07:22:29 +1000

    test asset url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="asset-url" content="{{ config('app.asset_url') }}">
 
     <title>{{ config('app.name', 'Seth Sharp Portfolio') }}</title>
+
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
