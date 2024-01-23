1679f4351603b3f2df99d51aee6bc5da839f56c7
Seth Sharp <sesharp@outlook.com>
2024-01-24 06:49:42 +1000

    break breeze routes into new blog route file

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,5 @@ Route::get('/experience', \App\Http\Controllers\Views\ShowExperienceController::
 Route::get('/capabilities', \App\Http\Controllers\Views\ShowCapabiltiesController::class)->name('capabilities');
 Route::get('/portfolio', \App\Http\Controllers\Views\ShowProjectsController::class)->name('projects');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 require __DIR__.'/auth.php';
+require __DIR__.'/blog.php';
