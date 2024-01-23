875cae13fefdfbb429371ef8534151ba1bc79dd1
Seth Sharp <sesharp@outlook.com>
2024-01-24 06:48:37 +1000

    install laravel breeze
    also re added some migrations which i deleted earlier and re-added the routes in web which were deleted

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,8 +11,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
@@ -18,3 +21,15 @@ Route::get('/about', \App\Http\Controllers\Views\ShowHomeController::class)->nam
 Route::get('/experience', \App\Http\Controllers\Views\ShowExperienceController::class)->name('experience');
 Route::get('/capabilities', \App\Http\Controllers\Views\ShowCapabiltiesController::class)->name('capabilities');
 Route::get('/portfolio', \App\Http\Controllers\Views\ShowProjectsController::class)->name('projects');
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,53 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
     ],
+
     theme: {
         extend: {
-            screens: {
-                "custom1": "845px",
-                "custom2": "960px",
-                "custom3": "979px"
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
-            transitionDuration: {
-                '0': '0ms',
-                '2000': '2000ms',
-            },
-            // that is animation class
-            animation: {
-                fade: 'fadeOut 0.3s ease-in-out',
-                rectUpFast: 'ru 4s linear infinite',
-                rectUpMed: 'ru 6s linear infinite',
-                rectUpSlow: 'ru 10s linear infinite',
-                rectDownFast: 'rd 4s linear infinite',
-                rectDownSlow: 'rd 8s linear infinite',
-            }, flex: {
-                'cus': '1 1 auto'
-            },
-            // that is actual animation
-            keyframes: theme => ({
-                fadeOut: {
-                    '0%': { border: "2px solid transparent" },
-                    '100%': { border: "2px solid black" },
-                }, ru: {
-                    '0%': {
-                        transform: "translateX(0px)",
-                        transform: "translateY(0px)"
-                    }, '100%': {
-                        transform: "translate(800px, -950px)"
-                    }
-                }, rd: {
-                    '0%': {
-                        transform: "translate(100px, -100px)"
-                    }, '100%': {
-                        transform: "translate(-500px,600px)"
-                    }
-                }
-            }),
         },
     },
-    plugins: [],
+
+    plugins: [require('@tailwindcss/forms')],
 };
