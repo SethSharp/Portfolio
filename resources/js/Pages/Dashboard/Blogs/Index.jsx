ba8e4e8b76fad10bfca0fb83dfb38408e3a07e4a
Seth Sharp <sesharp@outlook.com>
2024-01-26 11:16:17 +1000

    eslint formatting

--- resources/js/Pages/Dashboard/Blogs/Index.jsx
+++ resources/js/Pages/Dashboard/Blogs/Index.jsx
@@ -1,23 +1,21 @@
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.jsx";
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.jsx';
 
-export default function Index({auth, blogs}) {
-    return (
-        <AuthenticatedLayout
-            user={auth.user}
-        >
-            <div className="grid grid-cols-4">
-                {blogs.map((blog, index) => (
-                    <div key={index} className="bg-gray-200 p-8 rounded-xl">
-                        { blog.title  }
-                        <br />
-                        { blog.author.name}
+export default function Index({ auth, blogs }) {
+  return (
+    <AuthenticatedLayout user={auth.user}>
+      <div className="grid grid-cols-4">
+        {blogs.map((blog, index) => (
+          <div key={index} className="bg-gray-200 p-8 rounded-xl">
+            {blog.title}
+            <br />
+            {blog.author.name}
 
-                        <br />
-                        <br />
-                        { blog.content}
-                    </div>
-                ))}
-            </div>
-        </AuthenticatedLayout>
-    );
+            <br />
+            <br />
+            {blog.content}
+          </div>
+        ))}
+      </div>
+    </AuthenticatedLayout>
+  );
 }
