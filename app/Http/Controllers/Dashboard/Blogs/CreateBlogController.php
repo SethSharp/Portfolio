5c93c45583ae4c07fb7b11e8526fe08c9ca350b2
SethSharp <SethSharp@users.noreply.github.com>
2024-01-27 04:13:51 +0000

    Apply Code Style Fixes

--- app/Http/Controllers/Dashboard/Blogs/CreateBlogController.php
+++ app/Http/Controllers/Dashboard/Blogs/CreateBlogController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers\Dashboard\Blogs;
 
-use App\Http\Controllers\Controller;
 use Inertia\Inertia;
 use Inertia\Response;
+use App\Http\Controllers\Controller;
 
 class CreateBlogController extends Controller
 {
--- app/Http/Controllers/Dashboard/Blogs/UpdateBlogController.php
+++ app/Http/Controllers/Dashboard/Blogs/UpdateBlogController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers\Dashboard\Blogs;
 use App\Domain\Blog\Models\Blog;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
-use Inertia\Inertia;
-use Inertia\Response;
 
 class UpdateBlogController extends Controller
 {
