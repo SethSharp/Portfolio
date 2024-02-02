0e65c800412a9dcf5d62a64c10106f8328f51c70
Seth Sharp <sesharp@outlook.com>
2024-02-02 21:28:23 +1000

    testing configuration

---
+++ tests/Http/Dashboard/Blogs/StoreBlogTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dashboard\Blogs;
+
+use Tests\TestCase;
+
+class StoreBlogTest extends TestCase
+{
+    /** @test */
+    public function must_be_authenticated()
+    {
+        $this->markTestSkipped();
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,25 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Carbon\Carbon;
+use Tests\Traits\SeedsRoles;
+use Tests\Traits\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    use SeedsRoles;
     use RefreshDatabase;
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite();
+
+        $this->seedRoles();
+
+        Carbon::setTestNow(now());
+    }
 }
