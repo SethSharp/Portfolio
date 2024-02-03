d058bf432bb76d72cd1022038617a308b36028f0
Seth Sharp <sesharp@outlook.com>
2024-02-03 18:49:25 +1000

    push meta to layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
-    <title> Seth Sharp - Online Portfolio </title>
+    <title> @yield('title', config('app.name')) </title>
     <meta name="description"
           content="Hey I am Seth Sharp, a Junior Software developer at Coding Labs on the Gold Coast. I love to build ambitious projects and love the art of programming.">
 
--- resources/views/livewire/blogs/show-blog.blade.php
+++ resources/views/livewire/blogs/show-blog.blade.php
@@ -1,6 +1,18 @@
 @section('partOne', 'Blog')
 @section('partTwo', $blog->title)
 
+@section('title', $blog->meta_title ?: '')
+
+@push('meta')
+    @if($blog->meta_description)
+        <meta name="description" content="{{ $blog->meta_description }}">
+    @endif
+
+    @if($blog->meta_tags)
+        {!! $blog->meta_tags !!}
+    @endif
+@endpush
+
 @section("content")
-    {{ $blog->content }}
+    {!! $blog->content !!}
 @endsection
