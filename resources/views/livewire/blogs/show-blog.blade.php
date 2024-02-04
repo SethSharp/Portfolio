2dd22a9e4536cd81472c4bd64712610447b68863
Seth Sharp <sesharp@outlook.com>
2024-02-04 15:01:01 +1000

    wip

--- resources/views/livewire/blogs/show-blog.blade.php
+++ resources/views/livewire/blogs/show-blog.blade.php
@@ -1,6 +1,3 @@
-@section('partOne', 'Blog')
-@section('partTwo', $blog->title)
-
 @section('title', $blog->meta_title ?: '')
 
 @push('meta')
@@ -14,5 +11,18 @@
 @endpush
 
 @section("content")
-    {!! $blog->content !!}
+    <div class="w-3/4 mx-auto">
+        <div class="flex-wrap">
+            <div class="text-5xl font-bold"> {{ $blog->title }}</div>
+            <div class="text-gray-600 font-medium text-2xl"> {{ $blog->author->name  }}</div>
+
+            <div class="mt-2 text-gray-400 font-medium text-md">
+                Published: {{ $blog->created_at->diffForHumans() }}
+            </div>
+        </div>
+
+        <div class="mt-12 prose">
+            {!! $blog->content !!}
+        </div>
+    </div>
 @endsection
