92d9a06bd5382935fe98fc45c2f5714878df1f72
Seth Sharp <sesharp@outlook.com>
2024-02-21 07:08:51 +1000

    Create postdeploy.sh

---
+++ .platform/postdeploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo 'Running migrations'
+php artisan migrate:fresh --force
+
+echo 'Composer dump-autoload'
+composer dump-autoload
+
+echo 'Clearing cache'
+php artisan cache:clear
+php artisan config:clear
+php artisan view:clear
+php artisan route:clear
+php artisan config:cache
