d3f12485018f4a31d5dfdf99d37b3ed498dc1796
Seth Sharp <sesharp@outlook.com>
2024-02-20 12:20:29 +1000

    wip

--- app/Console/Commands/Bootstrap.php
+++ app/Console/Commands/Bootstrap.php
@@ -2,7 +2,11 @@
 
 namespace App\Console\Commands;
 
+use Illuminate\Support\Str;
+use App\Domain\Iam\Models\Role;
+use App\Domain\Iam\Models\User;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
 use App\Console\Commands\Bootstrap\Roles;
 
 class Bootstrap extends Command
@@ -15,5 +19,16 @@ class Bootstrap extends Command
         $this->info("Bootstrapping...");
 
         $this->call(Roles::class);
+
+        $user = User::create([
+            'name' => 'Seth Sharp',
+            'email' => 'sesharp@outlook.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('123456'),
+            'remember_token' => Str::random(10),
+        ]);
+
+        $admin = Role::whereName(User::ROLE_ADMIN)->first();
+        $user->roles()->attach([$admin->id]);
     }
 }
