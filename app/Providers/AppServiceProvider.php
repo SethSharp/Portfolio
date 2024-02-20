df2d8469d65992d3ca5a2cc623ab691c1c188bd4
SethSharp <SethSharp@users.noreply.github.com>
2024-02-20 09:04:54 +0000

    Apply Code Style Fixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Routing\UrlGenerator;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
