88aada78706ab1ce5aeced10e48e6931f819cb48
Seth Sharp <sesharp@outlook.com>
2024-02-20 18:59:43 +1000

    wip

--- .platform/predeploy.sh
+++ .platform/predeploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Navigate to the directory where your Vue.js project is
 cd /var/www/html
-
-# Run npm run build
-npm run build
 
 composer dump-autoload
 
-# Add more actions as needed
+php artisan cache:clear
+php artisan config:clear
+php artisan view:clear
+php artisan route:clear
+php artisan config:cache
+
+# Run npm run build
+npm run build
