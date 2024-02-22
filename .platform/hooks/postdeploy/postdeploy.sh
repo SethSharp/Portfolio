a6120d2ec3a469cf189e8240daca66145c066d06
Seth Sharp <sesharp@outlook.com>
2024-02-22 19:44:14 +1000

    Update postdeploy.sh

--- .platform/hooks/postdeploy/postdeploy.sh
+++ .platform/hooks/postdeploy/postdeploy.sh
@@ -2,10 +2,11 @@
 
 cd /var/www/html/
 
+php artisan key:generate
+
 php artisan cache:clear
 php artisan config:clear
 php artisan view:clear
 php artisan route:clear
-php artisan config:cache
 
-php artisan key:generate
+php artisan bootstrap
