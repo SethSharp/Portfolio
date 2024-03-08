3a30c8baaf57b1494d94f07a79b3c1f2e020f263
Seth Sharp <sesharp@outlook.com>
2024-03-08 19:28:52 +1000

    change to text

--- database/migrations/2024_03_03_003027_create_collections_table.php
+++ database/migrations/2024_03_03_003027_create_collections_table.php
@@ -15,7 +15,7 @@ return new class() extends Migration {
         Schema::create('collections', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->timestamps();
         });
     }
