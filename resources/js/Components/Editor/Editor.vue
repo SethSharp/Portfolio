5148e426901cd6c7984866ce88a3b2867d3096e5
Seth Sharp <sesharp@outlook.com>
2024-03-25 13:40:15 +1000

    text colour or a and p changes

--- resources/js/Components/Editor/Editor.vue
+++ resources/js/Components/Editor/Editor.vue
@@ -2,18 +2,19 @@
 import Link from '@tiptap/extension-link'
 import Code from '@tiptap/extension-code'
 import StarterKit from '@tiptap/starter-kit'
-import { Text } from '@tiptap/extension-text'
+import {Text} from '@tiptap/extension-text'
 import Document from '@tiptap/extension-document'
 import ListItem from '@tiptap/extension-list-item'
-import { Underline } from '@tiptap/extension-underline'
-import { HardBreak } from '@tiptap/extension-hard-break'
-import { useEditor, EditorContent } from '@tiptap/vue-3'
+import {Underline} from '@tiptap/extension-underline'
+import {HardBreak} from '@tiptap/extension-hard-break'
+import {useEditor, EditorContent} from '@tiptap/vue-3'
 import OrderedList from '@tiptap/extension-ordered-list'
-import { TextAlign } from '@tiptap/extension-text-align'
+import {TextAlign} from '@tiptap/extension-text-align'
 import Toolbar from '@/Components/Editor/Toolbar.vue'
 import BubbleMenu from '@/Components/Editor/BubbleMenu.vue'
 import Image from '@/Components/Editor/Nodes/Image/Image.js'
-import { Heading } from '@/Components/Editor/CustomExtensions/Heading.js'
+import {Heading} from '@/Components/Editor/CustomExtensions/Heading.js'
+import {Paragraph} from "@tiptap/extension-paragraph";
 
 const props = defineProps({
     modelValue: {
@@ -35,10 +36,15 @@ const editor = useEditor({
         }),
         Document,
         Heading,
+        Paragraph.configure({
+            HTMLAttributes: {
+                style: 'color: #475569;'
+            }
+        }),
         Link.configure({
             openOnClick: false,
             HTMLAttributes: {
-                style: 'color: black; text-decoration: underline black',
+                style: 'color: #1d4ed8;',
             },
         }),
         ListItem,
@@ -60,7 +66,7 @@ const editor = useEditor({
             class: 'bg-white p-4 max-w-none w-full min-h-[500px] focus:outline-none border border-gray-200 rounded-md max-h-[85vh] overflow-y-scroll overflow-hidden prose prose-img:m-0 ',
         },
     },
-    onUpdate: ({ editor }) => {
+    onUpdate: ({editor}) => {
         emits('update:modelValue', editor.getHTML())
     },
 })
@@ -69,17 +75,17 @@ const editor = useEditor({
 <template>
     <div>
         <div class="w-full bg-red-500">
-            <BubbleMenu :editor="editor" />
+            <BubbleMenu :editor="editor"/>
         </div>
 
-        <Toolbar :editor="editor" :blog="blog" />
+        <Toolbar :editor="editor" :blog="blog"/>
 
         <div class="mt-4">
             <div class="text-gray-500">
                 To add a hard break: `Ctr` + `Enter` on windows or `Cmd` + `Enter` on mac
             </div>
 
-            <EditorContent :editor="editor" />
+            <EditorContent :editor="editor"/>
         </div>
     </div>
 </template>
