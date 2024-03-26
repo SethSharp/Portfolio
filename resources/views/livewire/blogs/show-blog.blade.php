e17a90921627d5cf78cebcaf2c4643fa86f41461
Seth Sharp <sesharp@outlook.com>
2024-03-26 20:40:03 +1000

    replace with laravel intervention

--- resources/views/livewire/blogs/show-blog.blade.php
+++ resources/views/livewire/blogs/show-blog.blade.php
@@ -18,7 +18,6 @@
     <div class="flex-wrap">
 
         @if ($blog->cover_image)
-            asd
             <img src="{{ config('app.cloudfront_url') . $blog->cover_image }}"/>
         @endif
         <h1 class="text-2xl sm:text-4xl font-extrabold"> {{ $blog->title }}</h1>
