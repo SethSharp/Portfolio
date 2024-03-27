e791182e3b6e49b3e7f1b04a4bda8dcacf16bb74
Seth Sharp <sesharp@outlook.com>
2024-03-27 19:17:09 +1000

    wip

--- resources/views/components/blogs/card.blade.php
+++ resources/views/components/blogs/card.blade.php
@@ -1,28 +1,30 @@
-@props(['blog'])
-
 <div class="rounded-xl shadow-md overflow-hidden sm:flex h-full md:space-y-4">
     <div class="sm:h-full sm:w-1/2 p-2 md:p-4">
-        <img
-            src="{{ $blog->cover_image }}"
-            alt="{{ $blog->title . ' cover image' }}"
-            class="rounded-lg overflow-hidden object-cover aspect-square object-left lg:size-full max-h-52 md:max-h-80 m-auto"
-        />
+        <a href="{{ route('blogs.show', $blog)  }}">
+            <img
+                src="{{ $blog->cover_image }}"
+                alt="{{ $blog->title . ' cover image' }}"
+                class="rounded-lg overflow-hidden object-cover aspect-square object-left lg:size-full max-h-52 md:max-h-80 m-auto"
+            />
+        </a>
     </div>
 
     <div class="sm:w-1/2 text-left px-4 h-full md:py-2">
-        <div class="flex">
-            <p class="text-xs text-gray-400 font-medium my-auto"> {{ $blog->author->name  }} {{ $blog->published_at->diffForHumans() }} </p>
+        <a href="{{ route('blogs.show', $blog)  }}">
+            <div class="flex">
+                <p class="text-xs text-gray-400 font-medium my-auto"> {{ $blog->author->name  }} {{ $blog->published_at->diffForHumans() }} </p>
 
-            <div class="flex my-auto gap-0.5 ml-2 text-sm text-gray-400">
-                <x-icons.heart class="!size-3 my-auto"/>
-                <span> {{ $blog->likes->count() }}</span>
+                <div class="flex my-auto gap-0.5 ml-2 text-sm text-gray-400">
+                    <x-icons.heart class="!size-3 my-auto"/>
+                    <span> {{ $blog->likes->count() }}</span>
+                </div>
             </div>
-        </div>
 
-        <a href="{{ route('blogs.show', $blog)  }}" class="text-xl font-semibold"> {{ $blog->title }} </a>
+            <p class="text-xl font-semibold"> {{ $blog->title }} </p>
 
-        <div class="py-2 text-md text-secondary-400">
-            {{ $blog?->meta_description }}
-        </div>
+            <div class="py-2 text-md text-secondary-400">
+                {{ $blog?->meta_description }}
+            </div>
+        </a>
     </div>
 </div>
