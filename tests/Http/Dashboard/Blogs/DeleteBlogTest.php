b32e6875caf1d89771b41483b608e3ac51090ed6
Seth Sharp <sesharp@outlook.com>
2024-03-27 19:14:32 +1000

    tests

---
+++ tests/Http/Dashboard/Blogs/DeleteBlogTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Http\Dashboard\Blogs;
+
+use App\Providers\RouteServiceProvider;
+use Tests\TestCase;
+use App\Domain\Iam\Models\User;
+use App\Domain\Blog\Models\Blog;
+
+class DeleteBlogTest extends TestCase
+{
+    protected Blog $blog;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->blog = Blog::factory()->create();
+    }
+
+    /** @test */
+    public function must_be_authenticated()
+    {
+        $this->deleteJson(route('dashboard.blogs.delete', $this->blog))
+            ->assertStatus(401);
+    }
+
+    /** @test */
+    public function must_have_admin_role()
+    {
+        $this->actingAs(User::factory()->create())
+            ->deleteJson(route('dashboard.blogs.delete', $this->blog))
+            ->assertRedirect(RouteServiceProvider::BLOG);
+    }
+
+    /** @test */
+    public function can_delete_as_an_admin()
+    {
+        $this->actingAs(User::factory()->admin()->create())
+            ->deleteJson(route('dashboard.blogs.delete', $this->blog))
+            ->assertRedirect(route('dashboard.blogs.index'));
+    }
+}
