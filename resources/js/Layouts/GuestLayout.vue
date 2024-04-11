2088892b56c8e7252b95fd41c875d1db82bd8b70
Seth Sharp <sesharp@outlook.com>
2024-04-12 06:40:35 +1000

    add eb env to login

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -7,9 +7,11 @@ import { Link } from '@inertiajs/vue3'
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div>
             <a href="/">
-                <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+                <ApplicationLogo class="size-20 fill-current text-gray-500" />
             </a>
         </div>
+
+        <div class="text-gray-500 mt-2">Logging into {{ $page.props.auth.in_app_name }} site</div>
 
         <div
             class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
