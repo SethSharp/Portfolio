c79161668c789a99dcdf4732b8a89368acc8d259
Seth Sharp <sesharp@outlook.com>
2024-04-12 12:59:16 +1000

    integrate blog package v1.1.6

--- tests/Domain/File/Actions/StoreBlogCoverActionTest.php
+++ tests/Domain/File/Actions/StoreBlogCoverActionTest.php
@@ -19,8 +19,32 @@ class StoreBlogCoverActionTest extends TestCase
 
         $blog = Blog::factory()->create();
 
-        app(StoreBlogCoverAction::class)($file, $blog->id);
+        app(StoreBlogCoverAction::class)($file, $blog);
 
         Storage::disk('s3')->assertExists('testing/blogs/' . $blog->id . '/cover-images');
+    }
+
+    /** @test */
+    public function can_store_new_file_and_delete_old_one()
+    {
+        Storage::fake('s3');
+
+        $blog = Blog::factory()->create();
+
+        $oldFile = UploadedFile::fake()->image('old-file.jpg');
+        $newFile = UploadedFile::fake()->image('new-file.jpg');
+
+        $path = app(StoreBlogCoverAction::class)($oldFile, $blog);
+
+        $blog->update([
+            'cover_image' => $path
+        ]);
+
+        app(StoreBlogCoverAction::class)($newFile, $blog);
+
+        Storage::disk('s3')->assertMissing('testing/blogs/' . $blog->id . '/cover-images/' . $path);
+
+        $actualCount = count(Storage::disk('s3')->files('testing/blogs/' . $blog->id . '/cover-images'));
+        $this->assertEquals(1, $actualCount);
     }
 }
