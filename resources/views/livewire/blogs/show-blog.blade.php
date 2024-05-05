003e323d08871e5ea67ab3920f08fb39188bea1d
Seth Sharp <sesharp@outlook.com>
2024-05-06 09:36:53 +1000

    fix to next/prev blog posts on show-blog

--- resources/views/livewire/blogs/show-blog.blade.php
+++ resources/views/livewire/blogs/show-blog.blade.php
@@ -66,30 +66,30 @@
     </div>
 
     <div class="mx-auto grid lg:grid-cols-2 gap-6">
-        @if($prev)
+        @if($this->prev)
             <div
                 class="cursor-pointer rounded-lg border border-gray-300 transition hover:border-gray-500 hover:bg-gray-50 p-2">
-                <a href="{{ route('blogs.show', $prev) }}">
+                <a href="{{ route('blogs.show', $this->prev) }}">
                     <div class="flex">
                         <span class="text-primary-600">Previous blog </span>
                     </div>
 
-                    <div class="text-gray-700">{{ $prev->title }}</div>
+                    <div class="text-gray-700">{{ $this->prev->title }}</div>
                 </a>
             </div>
         @else
             <div></div>
         @endif
 
-        @if($next)
+        @if($this->next)
             <div
                 class="cursor-pointer rounded-lg border border-gray-300 transition hover:border-gray-500 hover:bg-gray-50 p-2">
-                <a href="{{ route('blogs.show', $next) }}">
+                <a href="{{ route('blogs.show', $this->next) }}">
                     <div class="flex justify-end">
                         <span class="text-primary-600">Next blog </span>
                     </div>
 
-                    <div class="flex justify-end text-right text-gray-700">{{ $this->blog->title }}</div>
+                    <div class="flex justify-end text-right text-gray-700">{{ $this->next->title }}</div>
                 </a>
             </div>
         @endif
