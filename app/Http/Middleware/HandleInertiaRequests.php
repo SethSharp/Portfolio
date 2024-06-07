670c5c222dd133dce79608c6939b47438ec93bbc
SethSharp <SethSharp@users.noreply.github.com>
2024-06-07 22:01:39 +0000

    Apply Code Style Fixes

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -40,8 +40,8 @@ class HandleInertiaRequests extends Middleware
                     'location' => $request->url(),
                 ]);
             },
-            'success' => fn() => $request->session()->get('success'),
-            'warning' => fn() => $request->session()->get('warning'),
+            'success' => fn () => $request->session()->get('success'),
+            'warning' => fn () => $request->session()->get('warning'),
         ]);
     }
 }
