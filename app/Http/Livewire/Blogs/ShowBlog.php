51025d5a72f5abd51b40e54cd81938fbad3a7e54
Seth Sharp <sesharp@outlook.com>
2024-06-16 10:48:38 +1000

    like implementation

--- app/Http/Livewire/Blogs/ShowBlog.php
+++ app/Http/Livewire/Blogs/ShowBlog.php
@@ -27,7 +27,7 @@ class ShowBlog extends Component
         $this->blogLikes = $this->blog->likes()->count();
 
         if (auth()->check()) {
-            $this->isLiked = auth()->user()->likedBlogs->contains('id', $this->blog->id);
+            $this->isLiked = auth()->user()->likes->contains('id', $this->blog->id);
         }
 
         $this->getSeries();
@@ -55,12 +55,14 @@ class ShowBlog extends Component
         }
 
         if ($this->isLiked) {
-            $this->blog->likes()->detach(auth()->user()->id);
+            $this->blog->likes()->where('user_id', auth()->user()->id)->delete();
 
             $this->isLiked = false;
             $this->blogLikes = $this->blogLikes - 1;
         } else {
-            $this->blog->likes()->attach(auth()->user()->id);
+            $this->blog->likes()->create([
+                'user_id' => auth()->user()->id
+            ]);
 
             $this->isLiked = true;
             $this->blogLikes = $this->blogLikes + 1;
