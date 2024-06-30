d08cd17509f67cb2aac63ab063a3b1759b6e6716
SethSharp <SethSharp@users.noreply.github.com>
2024-06-30 03:54:07 +0000

    Apply Code Style Fixes

--- routes/auth.php
+++ routes/auth.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
