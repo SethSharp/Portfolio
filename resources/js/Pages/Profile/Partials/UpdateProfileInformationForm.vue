661bba75cb7f6055bdbe8110e92db79f869811f5
SethSharp <SethSharp@users.noreply.github.com>
2024-07-01 03:37:15 +0000

    Apply Code Style Fixes

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -1,73 +1,73 @@
 <script setup>
-import {Link, useForm, usePage} from '@inertiajs/vue3'
-import {TextInput, FormElement, PrimaryButton} from '@sethsharp/ui'
+import { Link, useForm, usePage } from '@inertiajs/vue3'
+import { TextInput, FormElement, PrimaryButton } from '@sethsharp/ui'
 
 defineProps({
-  mustVerifyEmail: {
-    type: Boolean,
-  },
-  status: {
-    type: String,
-  },
+    mustVerifyEmail: {
+        type: Boolean,
+    },
+    status: {
+        type: String,
+    },
 })
 
 const user = usePage().props.auth.user
 
 const form = useForm({
-  name: user.name,
-  email: user.email,
+    name: user.name,
+    email: user.email,
 })
 </script>
 
 <template>
-  <section>
-    <header>
-      <h2 class="text-lg font-medium text-gray-900">Profile Information</h2>
+    <section>
+        <header>
+            <h2 class="text-lg font-medium text-gray-900">Profile Information</h2>
 
-      <p class="mt-1 text-sm text-gray-600">
-        Update your account's profile information and email address.
-      </p>
-    </header>
+            <p class="mt-1 text-sm text-gray-600">
+                Update your account's profile information and email address.
+            </p>
+        </header>
 
-    <form @submit.prevent="form.patch(route('profile.update'))" class="mt-6 space-y-6">
-      <FormElement>
-        <TextInput
-            id="name"
-            type="text"
-            v-model="form.name"
-            required
-            autocomplete="name"
-            label="Name"
-            :error="form.errors.name"
-            :show-character-count="false"
-        />
-      </FormElement>
+        <form @submit.prevent="form.patch(route('profile.update'))" class="mt-6 space-y-6">
+            <FormElement>
+                <TextInput
+                    id="name"
+                    type="text"
+                    v-model="form.name"
+                    required
+                    autocomplete="name"
+                    label="Name"
+                    :error="form.errors.name"
+                    :show-character-count="false"
+                />
+            </FormElement>
 
-      <FormElement>
-        <TextInput
-            id="email"
-            type="email"
-            v-model="form.email"
-            required
-            autocomplete="username"
-            label="Email"
-            :error="form.errors.email"
-            :show-character-count="false"
-        />
-      </FormElement>
+            <FormElement>
+                <TextInput
+                    id="email"
+                    type="email"
+                    v-model="form.email"
+                    required
+                    autocomplete="username"
+                    label="Email"
+                    :error="form.errors.email"
+                    :show-character-count="false"
+                />
+            </FormElement>
 
-      <div class="flex items-center gap-4">
-        <PrimaryButton :disabled="form.processing">Save</PrimaryButton>
+            <div class="flex items-center gap-4">
+                <PrimaryButton :disabled="form.processing">Save</PrimaryButton>
 
-        <Transition
-            enter-active-class="transition ease-in-out"
-            enter-from-class="opacity-0"
-            leave-active-class="transition ease-in-out"
-            leave-to-class="opacity-0"
-        >
-          <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Saved.</p>
-        </Transition>
-      </div>
-    </form>
-  </section>
+                <Transition
+                    enter-active-class="transition ease-in-out"
+                    enter-from-class="opacity-0"
+                    leave-active-class="transition ease-in-out"
+                    leave-to-class="opacity-0"
+                >
+                    <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Saved.</p>
+                </Transition>
+            </div>
+        </form>
+    </section>
 </template>
